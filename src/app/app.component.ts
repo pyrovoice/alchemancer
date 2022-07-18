fedd6fb53c34ed888144871bf808e8467bfcc2d6
username <maxime.grazzini@hotmail.com>
2022-07-18 23:49:35 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Exploration, ExplorationStatus } from './../model/exploration';
+import { Component, OnInit } from '@angular/core';
 
+enum Scene{
+  Fight,
+  Exploration
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Alchemancer';
+export class AppComponent implements OnInit{
+  currentScene: Scene = Scene.Fight;
+  Scene = Scene;
+  explorations: Exploration[] = [];
+
+  ngOnInit(): void {
+    this.explorations.push({id:"testLocked", name:"Locked", displayText:"I'm locked", status: ExplorationStatus.Locked, reward:{resources:[]}})
+    this.explorations.push({id:"testUnlocked", name:"Unlocked", displayText:"I'm Unlocked", status: ExplorationStatus.Unlocked, reward:{resources:[]}})
+  }
+
+  displayActionsScene(){
+    this.currentScene = Scene.Exploration
+  }
+
+  displayFightScene(){
+    this.currentScene = Scene.Fight
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SceneFightComponent } from './fighting/scene-fight/scene-fight.component';
+import { CharacterDetailComponent } from './fighting/character-detail/character-detail.component';
+import { ProgressBarComponent } from './shared-components/progress-bar/progress-bar.component';
+import { SceneExplorationComponent } from './exploration/scene-exploration/scene-exploration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SceneFightComponent,
+    CharacterDetailComponent,
+    ProgressBarComponent,
+    SceneExplorationComponent
   ],
   imports: [
     BrowserModule
